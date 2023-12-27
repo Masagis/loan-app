ed764ebea53ae85e106299e5b5c807e3ef5f5412
Agis Tri Wahyuji <agis.vendor.pasti@pegadaian.co.id>
2023-12-27 17:22:45 +0700

    feat(home): add chart

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <v-card>
     <v-card-title>
-      <h2>Home</h2>
+      <h3>Data Jumlah Pinjaman Berdasarkan Tujuan Pinjaman</h3>
     </v-card-title>
+    <v-card-text>
+      <apexcharts
+        width="100%"
+        height="350"
+        type="bar"
+        :options="chartData.chartOptions"
+        :series="chartData.series"
+      />
+    </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
-import { ApplicationModule } from '@/store/modules/application'
 import Vue from 'vue'
+import axios from 'axios'
+import VueApexCharts from 'vue-apexcharts'
 import { Component } from 'vue-property-decorator'
 
+import { ApplicationModule } from '@/store/modules/application'
+
+interface DataObject {
+  purpose: string;
+  amount: number;
+}
 @Component({
-  components: {}
+  name: 'chart',
+  components: { apexcharts: VueApexCharts }
 })
 export default class Home extends Vue {
   mounted () {
@@ -21,9 +38,62 @@ export default class Home extends Vue {
       {
         text: 'Home',
         disabled: true,
-        href: 'home'
+        href: '/home'
       }
     ])
+    this.getListPinjaman()
+  }
+
+  aggregatedData: DataObject[] = []
+
+  async getListPinjaman () {
+    try {
+      const response = await axios.get(
+        'https://raw.githubusercontent.com/andreascandle/p2p_json_test/main/api/json/loans.json'
+      )
+
+      response?.data?.forEach(
+        (element: { purpose: string; amount: number }) => {
+          const existingItem = this.aggregatedData.find(
+            (aggregatedItem) => aggregatedItem.purpose === element.purpose
+          )
+
+          if (existingItem) {
+            // If the purpose already exists, update the amount
+            existingItem.amount += element.amount
+          } else {
+            // If the purpose doesn't exist, add a new entry
+            this.aggregatedData.push({
+              purpose: element.purpose,
+              amount: element.amount
+            })
+          }
+        }
+      )
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  get chartData () {
+    return {
+      chartOptions: {
+        chart: {
+          id: 'basic-bar'
+        },
+        xaxis: {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          categories: this.aggregatedData?.map((bn: any) => bn?.purpose) ?? []
+        }
+      },
+      series: [
+        {
+          name: 'series-1',
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          data: this.aggregatedData?.map((bn: any) => bn?.amount) ?? []
+        }
+      ]
+    }
   }
 }
 </script>
--- src/pages/PinjamanView.vue
+++ src/pages/PinjamanView.vue
@@ -205,7 +205,8 @@ export default class Pinjaman extends Vue {
     }
   }
 
-  details (item) {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  details (item: any) {
     this.detailsData = item
     this.showData = false
     this.showDetail = true
